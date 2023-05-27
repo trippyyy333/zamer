c7e50dab4a76daa3504fd08227b0a57267158762
trippyyy333 <75495653+trippyyy333@users.noreply.github.com>
2023-05-27 14:48:09 -0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './a.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jssdfsf</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,9 +15,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactsec
         </a>
       </header>
+      <Navbar></Navbar>
     </div>
   );
 }
